b53dabc1d19881b14f47741b00cad4b4034f71d4
ylh96 <ylh96@10.0.0.243>
2016-10-09 22:59:47 -0700

    fyj

---
+++ src/Client.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ylh96
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int wind_oil_essential_oil = 0;
+	}
+
+}
